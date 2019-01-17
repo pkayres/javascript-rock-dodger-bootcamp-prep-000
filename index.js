5ed4a65b722cba8332f32f6ed2bdd8e1277355ba
Pete A <peter.ayresmusic@gmail.com>
2019-01-17 13:32:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -179,11 +179,11 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-   var rightNumbers = dodger.style.right.replace('px', '')
-   var right = parseInt(rightNumbers, 10)
+   var leftNumbers = dodger.style.left.replace('px', '')
+   var left = parseInt(leftNumbers, 10)
 
-   if (right < 360) {
-     dodger.style.right = `${right + 4}px`
+   if (left < 360) {
+     dodger.style.left = `${left + 4}px`
    }
   }
 
